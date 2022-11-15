0c2e75652f56c025a2b9ac7951a24154a95d4760
BOSSA Mickael <mickael.bossa@outlook.com>
2022-11-15 16:37:06 +0100

    Start article displays

--- src/containers/Articles/Articles.tsx
+++ src/containers/Articles/Articles.tsx
@@ -2,10 +2,32 @@
 import classes from './Articles.module.css';
 
 // Librairies
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from '../../config/axios-firebase';
+
+// Composant
+import DisplayedArticles from '../../components/DisplayedArticles/DisplayedArticles';
 
 export default function Articles() {
-  return (
-    <h1>Articles</h1>
-  )
+
+    // State
+
+    
+    // UseEffect
+    useEffect(() => {
+      axios.get('/articles.json')
+        .then(response => {
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        })
+    }, [])
+
+    return (
+        <>
+            <h1>Articles</h1>
+            <DisplayedArticles />
+        </>
+    );
 }
